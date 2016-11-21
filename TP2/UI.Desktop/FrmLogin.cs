86a995ee440700aadb7a4c7426434eb8bacf7014
prade516 <prade5_16@hotmail.com>
2016-11-21 01:37:58 -0300

    mejorar el login escritorio

--- TP2/UI.Desktop/FrmLogin.cs
+++ TP2/UI.Desktop/FrmLogin.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business.Entities;
+using Business.Logic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,16 +10,23 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using UI.Desktop;
+using Util;
 
 namespace UI.Desktop
 {
     public partial class FrmLogin : Form
     {
+        private string estado;
         public FrmLogin()
         {
             InitializeComponent();
         }
-
+        public enum gestion
+        {
+            Administrador = 1,
+            Profesor,
+            Alumno
+        }
         private void btnCancelar_Click(object sender, EventArgs e)
         {
             Application.Exit();
@@ -25,27 +34,64 @@ namespace UI.Desktop
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                Usuario person = new Usuario();
+                Validaciones valida = new Validaciones();
+                UsuarioLogic Logic = new UsuarioLogic();
+                List<Usuario> listadoPersonas = new List<Usuario>();
+                listadoPersonas = Logic.GetAll();
+                bool usuarioencontrado = false;
 
-            string usuario, password;
-            usuario = Convert.ToString(this.txtUsuario.Text);
-            password = Convert.ToString(this.txtContraseña.Text);
-            DataTable Datos = Business.Logic.UsuarioLogic.Login(usuario, password);
-            
-                if (Datos.Rows.Count == 0)
+                for (int i = 0; i < listadoPersonas.Count; i++)
                 {
-                    MessageBox.Show("No Tiene Acesso al Sistema", "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    if (!usuarioencontrado)
+                    {
+                        if (txtUsuario.Text.ToLower() == listadoPersonas[i].Nombre_Usuario.ToLower())
+                        {
+                            if (valida.EncryptKey(txtContraseña.Text) == listadoPersonas[i].Clave)
+                            {
+                                usuarioencontrado = true;
+                                person = listadoPersonas[i];
+                            }
+                        }
+                    }
+                    else
+                        break;
+                }
+                if (!usuarioencontrado)
+                {
+                    MessageBox.Show("No Tiene Acesso al Sistema", "Sistema Academico", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
                 else
                 {
                     Principal frm = new Principal();
-                    frm.IdUsuario = Datos.Rows[0][0].ToString();
-                    frm.Nombre = Datos.Rows[0][1].ToString();
-                    frm.Apellido = Datos.Rows[0][2].ToString();
-                    frm.Acceso = Datos.Rows[0][4].ToString();
+                    frm.IdUsuario = Convert.ToString(person.Id_Usuario);
+                    frm.Nombre = person.Nombre;
+                    frm.Apellido = person.Apellido;
+                    estado = person.Tipo;
+                    if (estado == Convert.ToString(gestion.Administrador))
+                    {
+                        frm.Acceso = "1";
+                    }
+                    else if (estado == Convert.ToString(gestion.Profesor))
+                    {
+                        frm.Acceso = "2";
+                    }
+                    else
+                    {
+                        frm.Acceso = "3";
+                    }
 
                     frm.Show();
                     this.Hide();
-                }                 
+
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            } 
           }
 
         private void FrmLogin_Load(object sender, EventArgs e)
